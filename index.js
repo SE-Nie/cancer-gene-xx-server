52bcc516fb3ee1244ffbb1f34d52001283d8a499
Ana Caballero <71068644+ansocab@users.noreply.github.com>
2021-01-19 16:57:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-
+const cookieParser = require("cookie-parser");
 var session = require('express-session')
 const cors = require("cors");
 port = process.env.PORT || 4000
 
 app.use(cors({credentials: true, origin: 'https://focused-sinoussi-d2664b.netlify.app'}));
 app.use(express.json());
-
+app.use(cookieParser());
 app.use(
   session({
     resave: false,
